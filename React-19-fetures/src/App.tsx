070a628849edd540626f90285b9bc57e8fc20c49
ashrafali05 <ashrafmohd83517@gmail.com>
2026-05-22 06:08:33 +0530

    commit

--- React-19-fetures/src/App.tsx
+++ React-19-fetures/src/App.tsx
@@ -3,7 +3,34 @@
 // import UseFormStatusForm from "./Components/UseFormStatusForm";
 // import ActionsForm from "./Components/ActionsForm";
 
+import { useState, useTransition } from "react";
+import Home from "./Components/useTransitionHook/Home";
+import Post from "./Components/useTransitionHook/Post";
+import Contact from "./Components/useTransitionHook/Contact";
+
 const App = () => {
+  const [activeTab, setActiveTab] = useState("home");
+  const [isPending, startTransition] = useTransition();
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "home":
+        return <Home />;
+      case "post":
+        return <Post />;
+      case "contact":
+        return <Contact />;
+      default:
+        return <Home />;
+    }
+  };
+
+  const handleTabChange = (tab: any) => {
+    startTransition(() => {
+      setActiveTab(tab);
+    });
+  };
+
   return (
     <div>
       {/* <Suspense fallback={<h1>Loading...</h1>}>
@@ -11,6 +38,29 @@ const App = () => {
       </Suspense> */}
       {/* <ActionsForm /> */}
       {/* <UseFormStatusForm /> */}
+
+      <div className="tabs">
+        <button
+          className="border-2 border-gray-200 ml-5 px-4 py-2 cursor-pointer"
+          onClick={() => handleTabChange("home")}
+        >
+          Home
+        </button>
+        <button
+          className="border-2 border-gray-200 ml-5 px-4 py-2 cursor-pointer"
+          onClick={() => handleTabChange("contact")}
+        >
+          Contact
+        </button>
+        <button
+          className="border-2 border-gray-200 ml-5 px-4 py-2 cursor-pointer"
+          onClick={() => handleTabChange("post")}
+        >
+          Post
+        </button>
+        {isPending && <h1>Loading...</h1>}
+        <div className="content"> {renderContent()} </div>
+      </div>
     </div>
   );
 };
