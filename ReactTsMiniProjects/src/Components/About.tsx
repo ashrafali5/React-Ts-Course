ab9bc438a4699a74fcb0b15ba96552ea7a6acdc9
ashrafali5 <ashrafali095y@gmail.com>
2026-06-10 09:44:16 +0530

    React 19 deleted

--- ReactTsMiniProjects/src/Components/About.tsx
+++ ReactTsMiniProjects/src/Components/About.tsx
@@ -1,8 +1,22 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto">
+        <h2 className="text-gray-900 font-extrabold text-3xl mb-6">
+          About Ashraf_Ali
+        </h2>
+        <p className="text-gray-600 text-lg mb-4">
+          Ashraf_Ali is a passionate software developer with a love for creating
+          innovative solutions. With a background in computer science and a keen eye for design, Ashraf_Ali has a knack for building user-friendly applications that solve real-world problems.
+        </p>
+        <p className="text-gray-600 text-lg mb-4">
+          With experience in various programming languages and frameworks, Ashraf_Ali has worked on a wide range of projects, from web applications to mobile apps. Ashraf_Ali is always eager to learn new technologies and stay up-to-date with the latest trends in the industry.
+        </p>
+        <p className="text-gray-600 text-lg">
+          When not coding, Ashraf_Ali enjoys exploring new places, trying out different cuisines, and spending time with friends and family. Ashraf_Ali is also an avid gamer and loves to unwind with a good video game after a long day of coding.
+        </p>
+      </div>
+    </section>
   );
 };
 
--- ReactTsMiniProjects/src/Components/Card.tsx
+++ ReactTsMiniProjects/src/Components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 const Card = ({ title, description, image }: CardProps) => {
   return (
-    <div className="max-w-sm mx-auto m-4 rounded-lg shadow-md overflow-hidden bg-gray-100 ">
+    <div className="max-w-sm mx-auto m-4 rounded-lg shadow-md overflow-hidden">
       <img src={image} alt={title} className="w-full h-48 object-cover" />
 
       <div className="p-6">
